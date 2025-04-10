72e497c32fa137207b63fe10df86a26c9b5e162d
G0ldenDragon <149322207+G0ldenDragon@users.noreply.github.com>
2025-04-10 21:33:34 +0200

    Début de la construction du gestionnaire d'emails

--- Models/EmailGestionnary_Model.py
+++ Models/EmailGestionnary_Model.py
@@ -1,2 +1,30 @@
+from Constants import CONFIGURATION_STMP
 
-def validate_email
+from validate_email import validate_email
+
+class EmailGestionnary_Model:
+    def __init__(self):
+        self.emailSender = ""
+
+
+    def set_email_sender(self, emailSender: str):
+        if self.is_email_valid:
+            self.emailSender = emailSender
+        else:
+            raise Exception(f"The email {emailSender} provided is not valid")
+
+
+    def get_email_sender(self) -> str:
+        return self.emailSender
+
+
+    def is_email_valid(self, email: str) -> bool:
+        for provider, config in CONFIGURATION_STMP.items():
+            if provider in email:
+               if validate_email(email):
+                   return True
+
+        return False
+
+
+    # def send_email(self, email):
